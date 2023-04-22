4d2b531de0af6d9c42683f32928403d66fbfc628
arifsrepo <arif.me.40943@gmail.com>
2023-04-22 16:11:24 +0600

    4th commit

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function server() {
             }
             res.header("Access-Control-Allow-Origin", "*");
             res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
             res.json(data);
         })
     }
